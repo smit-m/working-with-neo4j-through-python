a0ad477583321271582de8c8f77c398056bb15d4
smit-m <mehta.smit92@gmail.com>
2019-08-19 15:53:58 -0400

    Update csv_to_neo4j_v1.py

--- csv_to_neo4j_v1.py
+++ csv_to_neo4j_v1.py
@@ -29,46 +29,49 @@ def main():
             j = 0;
             for row in reader:    
                 if row:
-                    character = strip(row[0])
-                    first_name = strip(row[1])
-                    last_name = strip(row[2])
-                    actor = strip(row[3])
-                    character_birth = strip(row[4])
-                    character_death = strip(row[5])
-                    allegiance = strip(row[6])
-                    house = strip(row[7])
-                    territory = strip(row[8])
-                    region = strip(row[9])
+                    userid = strip(row[0])
+                    brand = strip(row[1])
+                    category = strip(row[2])
+                    url = strip(row[3])
+                    site = strip(row[4])
+                    price = strip(row[5])
+                    productName = strip(row[6])
+                    productid = strip(row[7])
+                    eventtype = strip(row[8])
+                    
                     query = """
-                        merge (character:Character {Character: {a}, First_Name:{b}, Last_Name:{c}, Actor:{d}, Birth:{e}, Death:{f}})
-                        merge (house:House{House:{g}, Allegiance:{h}})
-                        merge (territory:Territory {Territory: {i}})
-                        merge (region:Region {Region: {j}})
-                        merge (character)-[:Of_House{House:{k}}]-(house)-[:Is_From]->(territory)-[:Is_In]->(region)
+                        merge (userid:User {User: {u}})
+                        merge (brand:Brand{Brand:{b}})
+                        merge (productName:productName {productName: {pn}})
+                        
+                        merge (u)-[:Clicked_ON]->(pn)-[:Is_Made_By]->(b)
                     """
-                    batch.append(query, {"a":character, "b": first_name, "c": last_name, "d":actor, "e":character_birth, "f":character_death, "g":house, "h": allegiance, "i":territory, "j":region, "k":house})
+                    batch.append(query, {"u":userid, "b": brand, "pn": productName})
                     i += 1
                     j += 1
                 batch.process()
 
-                if (i == 1000): #submits a batch every 1000 lines read
+                if (i == 2): #submits a batch every 2 lines read
                     batch.commit()
-                    print j, "lines processed"
+                    print(str(j) + "lines processed")
                     i = 0                
                     batch = graph.cypher.begin()
-            else: batch.commit() #submits remainder of lines read                       
-            print j, "lines processed"     
+                else: 
+                    batch.commit() #submits remainder of lines read                       
+                    print(str(j) + "lines processed")  
 
         except Exception as e:
-            print e, row, reader.line_num
+            #print e, row, reader.line_num
+            print("Err")
 
-def strip(string): return''.join([c if 0 < ord(c) < 128 else ' ' for c in string]) #removes non utf-8 chars from string within cell
+def strip(string): 
+    return''.join([c if 0 < ord(c) < 128 else ' ' for c in string]) #removes non utf-8 chars from string within cell
 
 if __name__ == '__main__':
     start = time.time()
     main()
     end = time.time() - start
-    print "Time to complete:", end
+    print("Time to complete: " + end)
 
 
 
