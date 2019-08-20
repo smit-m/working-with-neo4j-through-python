19da1d67d33aebf2d31032ddc315ea2d05e002dc
smit-m <mehta.smit92@gmail.com>
2019-08-20 12:11:05 -0400

    Update getting_started_with_py2neo.py

---
+++ getting_started_with_py2neo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+#########################################################################
+### Authors: Smit Mehta
+### Python:  Version 3.7
+### Details: Getting started with py2neo using the resource: Py2neo v3 Handbook (https://py2neo.org/v3/index.html)
+#########################################################################
+
+
